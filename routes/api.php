570c1b5eea992d1d537d09a8c65b2cb06589f82b
axad03213 <axad03213@gmail.com>
2022-03-04 19:22:47 +0500

    learnig controller and models and views

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,11 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+/*
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+*/
+Route::get('/users',function(){
+    return "users";
 });
